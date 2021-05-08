25335794f856db58b2a33220f807a78f92d6d0b7
Anish Sharma <aneeshsharma@outlook.com>
2021-05-08 21:07:13 +0530

    updated names

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='PyComm',
-    packages=find_packages(include=['PyComm']),
+    name='pycomm',
+    packages=find_packages(include=['pycomm']),
     version='0.1.0',
     description='An encrypted TCP communication libarary',
     author='Anish Sharma',
-    license='GPLv3-or-later',
+    license='GPLv3.0-or-later',
     install_requires=['cryptography']
 )
